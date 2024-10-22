1c8c34a2dfdf1fff55e799f9b37c19f2e6387aec
riddlerxenon <riddler.xenon@gmail.com>
2024-10-22 15:37:56 +0300

    some changes

--- internal/integral/methods/rectangles.go
+++ internal/integral/methods/rectangles.go
@@ -4,7 +4,7 @@ func LeftRectangleMethod(a, b, n float64, expr string) (float64, error) {
 	h := (b - a) / n
 	sum := 0.0
 
-	for i := 0.0; i < n; i += 0.1 {
+	for i := 0.0; i < n; i++ {
 		x := a + i*h
 		fx, err := f(x, expr)
 		if err != nil {
@@ -16,27 +16,27 @@ func LeftRectangleMethod(a, b, n float64, expr string) (float64, error) {
 	return sum * h, nil
 }
 
-func RightRectangleMethod(a, b float64, n float64, expr string) (float64, error) {
-	h := (b - a) / float64(n)
-	sum := 0.0
-
-	for i := 1.0; i <= n; i += 0.1 {
-		x := a + i*h
-		fx, err := f(x, expr)
-		if err != nil {
-			return 0, err
-		}
-		sum += fx
-	}
-
-	return sum * h, nil
-}
-
-func MidpointRectangleMethod(a, b float64, n float64, expr string) (float64, error) {
+func RightRectangleMethod(a, b, n float64, expr string) (float64, error) {
 	h := (b - a) / n
 	sum := 0.0
 
-	for i := 0.0; i < n; i += 0.1 {
+	for i := 0.0; i < n; i++ {
+		x := a + i*h
+		fx, err := f(x, expr)
+		if err != nil {
+			return 0, err
+		}
+		sum += fx
+	}
+
+	return sum * h, nil
+}
+
+func MidpointRectangleMethod(a, b, n float64, expr string) (float64, error) {
+	h := (b - a) / n
+	sum := 0.0
+
+	for i := 0.0; i < n; i++ {
 		x := a + (i+0.5)*h
 		fx, err := f(x, expr)
 		if err != nil {
