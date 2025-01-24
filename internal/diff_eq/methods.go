f486fc3270b06528a3bf2462aeff0dfa61602c8b
Максим Токарев <maxim.tokaref@gmail.com>
2025-01-24 19:06:40 +0300

    added differential equation, updated parser

---
+++ internal/diff_eq/methods.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/RiddlerXenon/Integralize/internal/parser"
+)
+
+func eulerMethod(f func(float64, float64) float64, y0, t0, tMax, h float64) ([]float64, []float64) {
+	//Разбиваем на шаги
+	nSteps := int((tMax - t0) / h)
+
+	/* Так как изначально диффур принимает вид как dy/dt,
+	то логично использовать X = t, Y = y */
+	t := make([]float64, nSteps+1)
+	y := make([]float64, nSteps+1)
+
+	t[0] = t0 //Начальное время
+	y[0] = y0 //y(0) = Введенному значению
+
+	/* Воспользуемся формулой
+	y[i + 1] = y[i] - h * k * y[i]
+	t[i + 1] = t[i] + h
+	где k - коэффициент, y0 - начальное значение
+	tMax - максимальное время, h - шаг */
+	for i := 0; i < nSteps; i++ {
+		t[i+1] = t[i] + h
+		y[i+1] = y[i] + h*f(t[i], y[i])
+	}
+
+	return t, y
+}
+
+func rungeKutte(f func(float64, float64) float64, y0, t0, tMax, h float64) ([]float64, []float64) {
+	nSteps := int((tMax - t0) / h)
+	t := make([]float64, nSteps+1)
+	y := make([]float64, nSteps+1)
+
+	t[0] = t0
+	y[0] = y0
+
+	for i := 0; i < nSteps; i++ {
+		t[i+1] = t[i] + h
+
+		k1 := h * f(t[i], y[i])
+		k2 := h * f(t[i]+h/2, y[i]+k1/2)
+		k3 := h * f(t[i]+h/2, y[i]+k2/2)
+		k4 := h * f(t[i]+h, y[i]+k3)
+
+		y[i+1] = y[i] + (k1+2*k2+2*k3+k4)/6
+	}
+
+	return t, y
+}
+
+func main() {
+	eqStr := "X + sin(Y)"
+
+	parsFunc, err := parser.ParseStrDiffEq(eqStr)
+	if err != nil {
+		log.Fatalf("Ошибка парсинга: %v", err)
+	}
+
+	//Начальные условия для примера
+	y0 := 1.0
+	t0 := 0.0
+	tMax := 2.0
+	h := 0.1
+
+	t, y := eulerMethod(parsFunc, y0, t0, tMax, h)
+	t1, y1 := rungeKutte(parsFunc, y0, t0, tMax, h)
+
+	fmt.Printf("Метод Эйлера\n")
+	for i := range t {
+		fmt.Printf("t=%.2f, y=%.4f\n", t[i], y[i])
+	}
+
+	fmt.Printf("\nМетод Рунге-Кутты\n")
+	for i := range t {
+		fmt.Printf("t=%.2f, y=%.4f\n", t1[i], y1[i])
+	}
+}
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-func ParseStr(str string) (func(float64) float64, error) {
+func ParseStrInt(str string) (func(float64) float64, error) {
 	funcs := map[string]func(float64) float64{
 		"sin":  math.Sin,
 		"cos":  math.Cos,
@@ -116,8 +116,142 @@ func ParseStr(str string) (func(float64) float64, error) {
 					}
 					j++
 				}
-				subFunc, _ := ParseStr(str[i+1 : j-1])
+				subFunc, _ := ParseStrInt(str[i+1 : j-1])
 				push(subFunc(x))
+				i = j - 1
+			}
+		}
+
+		// Применение оставшихся операторов
+		for len(operators) > 0 {
+			applyOperator(&stack, operators[len(operators)-1])
+			operators = operators[:len(operators)-1]
+		}
+
+		if len(stack) == 1 {
+			return stack[0]
+		}
+		return 0
+	}, nil
+}
+
+func ParseStrDiffEq(str string) (func(float64, float64) float64, error) {
+	funcs := map[string]func(float64) float64{
+		"sin":  math.Sin,
+		"cos":  math.Cos,
+		"tan":  math.Tan,
+		"exp":  math.Exp,
+		"ln":   math.Log,
+		"sqrt": math.Sqrt,
+	}
+
+	// Функция для выполнения операций на стеке
+	applyOperator := func(stack *[]float64, operator byte) error {
+		if len(*stack) < 2 {
+			return errors.New("недостаточно операндов")
+		}
+		b, a := (*stack)[len(*stack)-1], (*stack)[len(*stack)-2]
+		*stack = (*stack)[:len(*stack)-2]
+
+		switch operator {
+		case '+':
+			*stack = append(*stack, a+b)
+		case '-':
+			*stack = append(*stack, a-b)
+		case '*':
+			*stack = append(*stack, a*b)
+		case '/':
+			*stack = append(*stack, a/b)
+		case '^':
+			*stack = append(*stack, math.Pow(a, b))
+		}
+		return nil
+	}
+
+	// Основная функция для вычисления
+	return func(x float64, y float64) float64 {
+		var (
+			stack     []float64
+			operators []byte
+			applyFunc func(float64) float64
+		)
+
+		push := func(val float64) {
+			if applyFunc != nil {
+				val = applyFunc(val)
+				applyFunc = nil
+			}
+			stack = append(stack, val)
+		}
+
+		for i := 0; i < len(str); i++ {
+			char := str[i]
+
+			// Пропуск пробелов
+			if char == ' ' {
+				continue
+			}
+
+			// Если встречаем оператор
+			if char == '+' || char == '-' || char == '*' || char == '/' || char == '^' {
+				// Проверка приоритета операторов
+				for len(operators) > 0 && getPrecedence(operators[len(operators)-1]) >= getPrecedence(char) {
+					applyOperator(&stack, operators[len(operators)-1])
+					operators = operators[:len(operators)-1]
+				}
+				operators = append(operators, char)
+				continue
+			}
+
+			// Обработка переменной x
+			if char == 'X' {
+				push(x)
+				continue
+			}
+
+			if char == 'Y' {
+				push(y)
+				continue
+			}
+
+			// Обработка функций
+			if char >= 'a' && char <= 'z' {
+				j := i
+				for j < len(str) && str[j] >= 'a' && str[j] <= 'z' {
+					j++
+				}
+				if f, ok := funcs[str[i:j]]; ok {
+					applyFunc = f
+					i = j - 1
+				}
+				continue
+			}
+
+			// Обработка чисел
+			if (char >= '0' && char <= '9') || char == '.' {
+				j := i
+				for j < len(str) && ((str[j] >= '0' && str[j] <= '9') || str[j] == '.') {
+					j++
+				}
+				num, _ := strconv.ParseFloat(str[i:j], 64)
+				push(num)
+				i = j - 1
+				continue
+			}
+
+			// Обработка подвыражений в скобках
+			if char == '(' {
+				j, depth := i+1, 1
+				for j < len(str) && depth > 0 {
+					if str[j] == '(' {
+						depth++
+					} else if str[j] == ')' {
+						depth--
+					}
+					j++
+				}
+				subFunc, _ := ParseStrDiffEq(str[i+1 : j-1])
+				push(subFunc(x, y))
 				i = j - 1
 			}
 		}
@@ -149,28 +283,28 @@ func getPrecedence(op byte) int {
 	}
 }
 
-// func main() {
-// 	numRuns := 100
-// 	totalDuration := time.Duration(0)
+/* func main() {
+	numRuns := 100
+	totalDuration := time.Duration(0)
 
-// 	var startMem, endMem runtime.MemStats
-// 	runtime.ReadMemStats(&startMem)
+	var startMem, endMem runtime.MemStats
+	runtime.ReadMemStats(&startMem)
 
-// 	for i := 0; i < numRuns; i++ {
-// 		start := time.Now()
-// 		exprFunc, err := ParseStr("2^3/1^4")
-// 		if err != nil {
-// 			fmt.Println("Error:", err)
-// 			return
-// 		}
-// 		fmt.Println(exprFunc(0.5))
-// 		totalDuration += time.Since(start)
-// 	}
-// 	runtime.ReadMemStats(&endMem)
+	for i := 0; i < numRuns; i++ {
+		start := time.Now()
+		exprFunc, err := ParseStrDiffEq("X^3/Y^4")
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		fmt.Println(exprFunc(2, 3))
+		totalDuration += time.Since(start)
+	}
+	runtime.ReadMemStats(&endMem)
 
-// 	avgDuration := totalDuration / time.Duration(numRuns)
-// 	fmt.Println("Среднее время выполнения:", avgDuration.Nanoseconds())
+	avgDuration := totalDuration / time.Duration(numRuns)
+	fmt.Println("Среднее время выполнения:", avgDuration.Nanoseconds())
 
-// 	memoryUsage := endMem.Alloc - startMem.Alloc
-// 	fmt.Println("Используемая память (в среднем за тест):", memoryUsage, "байт")
-// }
+	memoryUsage := endMem.Alloc - startMem.Alloc
+	fmt.Println("Используемая память (в среднем за тест):", memoryUsage, "байт")
+} */
